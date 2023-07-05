7f5e7f9b160373e12736dd1ec2e690b936857ca8
Usaid Asif <usaidasif12@gmail.com>
2023-07-05 14:37:03 +0500

    set up the project & added container and Image

---
+++ lib/data/data.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+//Update with colors of your choice for dp background gradient
+Color kGradient1 = Colors.black;
+Color kGradient2 = Colors.blueGrey;
+
+String imagePath = "images/Mypic.png";
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../data/data.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    kGradient1,
+                    kGradient2,
+                  ],
+                ),
+              ),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage(imagePath),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
